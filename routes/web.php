9757d0a9d8a4b38a4af7d903fb0d6560e1a5be6a
AngeeG28 <cami28g@gmail.com>
2025-12-16 21:11:58 -0500

    Proyecto laravel inventario productos

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<!-- Se ponen todas las rutas -->
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+ 
+// Instanciar el controlador, cada que se haga uno nuevo debe de aplicarse, para llamarlo:
+use App\Http\Controllers\ProductosController; 
+
+// RUTA LLAMANDO UNA VISTA:
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// RUTA POR MEDIO DE UN CONTROLADOR:
+Route::get('/productos', [ProductosController::class, 'productos']);
+
+// RUTA DEVUELVE CADENA DE TEXTO:
+Route::get('/nosotros', function () {
+    return "Estos domos nosotros";
+});
+
+Route::get('/detalle/{id}', function ($id) {
+    return view('detalle', compact('id'));
+});
+
+
+// EJEMPLO
+// Route::get('/detalle', function () {
+//     return view('detalle');
+// });
